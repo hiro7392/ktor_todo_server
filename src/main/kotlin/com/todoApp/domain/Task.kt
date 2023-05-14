03d6fe0a6da41c71e73621ef768d1d66be82a4d5
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2023-05-14 17:55:46 +0900

    Taskドメインにいくつか振る舞いを追加

--- src/main/kotlin/com/todoApp/domain/Task.kt
+++ src/main/kotlin/com/todoApp/domain/Task.kt
@@ -4,12 +4,49 @@ import java.time.LocalDateTime
 
 
 data class Task(
-    var id: Long,
-    var title: String,
-    var description: String?,
-    var deadline: LocalDateTime,
-    var completed:  Boolean,
-    var createdAt:LocalDateTime,
-    var updatedAt:LocalDateTime,
+    val id: String,
+    val title: String,
+    val description: String?,
+    val deadline: LocalDateTime,
+    val completed:  Boolean,
+    val createdAt:LocalDateTime,
+    val updatedAt:LocalDateTime,
+){
+    init {
 
-)
+    }
+    fun complete():Task{
+        return Task(
+            this.id,
+            this.title,
+            this.description,
+            deadline,
+            completed=true,
+            createdAt,
+            updatedAt,
+        )
+    }
+    fun updateDeadline( newDeadline:LocalDateTime):Task{
+        return Task(
+            this.id,
+            this.title,
+            this.description,
+            newDeadline,
+            completed,
+            createdAt,
+            updatedAt,
+        )
+    }
+    fun updateDescription( newDescription:String):Task{
+        return Task(
+            this.id,
+            this.title,
+            newDescription,
+            deadline,
+            completed,
+            createdAt,
+            updatedAt,
+        )
+    }
+
+}
